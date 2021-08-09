2845d6f9dc853327bf5646f8989c7f2d801ad05d
Anri <anri0806@hotmail.co.jp>
2021-08-08 22:31:38 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "<h1>He is the champion</h1>"
